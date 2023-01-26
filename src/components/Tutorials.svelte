ab7f14b4391314214a93b5076d3ffe507a42476a
xzhuo <yunkzo@gmail.com>
2023-01-26 10:58:27 -0600

    add tutorials

---
+++ src/components/Tutorials.svelte
@@ -0,0 +1,43 @@
+<div>
+    <main>
+        <section class=" py-16 bg-blueGray-200">
+            <div class="container mx-auto px-4">
+                <div class=" flex flex-col min-w-0 break-words bg-white w-full mb-6 shadow-xl rounded-lg mt-64">
+                    <div class="px-6">
+                        <div class="text-center mt-12">
+                            <h3 class="text-4xl font-semibold leading-normal text-gray-700 mb-2">Showcases</h3>
+                        </div>
+                        <div class="mt-10 py-10 border-t">
+                            <div class="flex flex-wrap justify-center">
+                                <div class="w-full leading-normal text-gray-700 mb-2">
+                                    <ol>
+                                        <li>
+                                            <a
+                                                href="https://www.youtube.com/watch?v=yCBGsUNjQW0"
+                                                target="_blank"
+                                                >Selection species and assemblies in the WashU Comparative Epigenome Browser</a
+                                            >
+                                            <br />
+                                            How to use the WashU Comparative Epigenome browser to select species and assemblies.
+                                        </li>
+                                        <li>
+                                            <a
+                                                href="https://www.youtube.com/watch?v=FvUtJGLWdRs"
+                                                target="_blank"
+                                                >how to add new data tracks to the WashU Comparative Epigenome Browser and basic navigation. </a
+                                            >
+                                            <br />
+                                            how to add new data tracks to the WashU Comparative Epigenome Browser from either a link or 
+                                                a local file and perform basic navigation within the browser.
+                                        </li>
+                                    </ol>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+    </main>
+    <!-- <Footer /> -->
+</div>
