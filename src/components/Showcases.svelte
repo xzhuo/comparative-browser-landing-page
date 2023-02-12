a139054ee337ef4dada60a0f84fe15ba285db4e2
xzhuo <yunkzo@gmail.com>
2023-02-12 17:21:38 -0600

    add json file download link and figure 7

--- src/components/Showcases.svelte
+++ src/components/Showcases.svelte
@@ -21,6 +21,12 @@
                                             From Zhang, Chi, Yujin Hoshida, and Kirsten C. Sadler. “Comparative Epigenomic Profiling
                                             of the DNA Methylome in Mouse and Zebrafish Uncovers High Interspecies Divergence.”
                                             Frontiers in Genetics 7 (June 17, 2016). https://doi.org/10.3389/fgene.2016.00110.
+                                            <br />
+                                            <a
+                                                href="https://vizhub.wustl.edu/public/comparativeBrowser/demo/Fig3a.session.json"
+                                                target="_blank"
+                                                >Session file</a
+                                            >
                                         </li>
                                         <li>
                                             <a
@@ -34,6 +40,12 @@
                                             and Yue, Feng, Yong Cheng, Alessandra Breschi, Jeff Vierstra, Weisheng Wu, Tyrone Ryba,
                                             Richard Sandstrom, et al. “A Comparative Encyclopedia of DNA Elements in the Mouse Genome.”
                                             Nature 515, no. 7527 (November 2014): 355–64. https://doi.org/10.1038/nature13992.
+                                            <br />
+                                            <a
+                                                href="https://vizhub.wustl.edu/public/comparativeBrowser/demo/Fig3b.session.json"
+                                                target="_blank"
+                                                >Session file</a
+                                            >
                                         </li>
                                         <li>
                                             <a
@@ -46,6 +58,12 @@
                                             Iñigo Narvaiza, Licia Selleri, Fred H Gage, Tomek Swigut, and Joanna Wysocka.
                                             “Enhancer Divergence and Cis-Regulatory Evolution in the Human and Chimp Neural Crest.”
                                             CELL 163, no. 1 (September 2015): 68–83. https://doi.org/10.1016/j.cell.2015.08.036.
+                                            <br />
+                                            <a
+                                                href="https://vizhub.wustl.edu/public/comparativeBrowser/demo/Fig4a.session.json"
+                                                target="_blank"
+                                                >Session file</a
+                                            >
                                         </li>
                                         <li>
                                             <a
@@ -57,6 +75,12 @@
                                             From Yang, Yang, Yang Zhang, Bing Ren, Jesse R. Dixon, and Jian Ma.
                                             “Comparing 3D Genome Organization in Multiple Species Using Phylo-HMRF.”
                                             Cell Systems 8, no. 6 (June 26, 2019): 494-505.e14. https://doi.org/10.1016/j.cels.2019.05.011.
+                                            <br />
+                                            <a
+                                                href="https://vizhub.wustl.edu/public/comparativeBrowser/demo/Fig4b.session.json"
+                                                target="_blank"
+                                                >Session file</a
+                                            >
                                         </li>
                                         <li>
                                             <a
@@ -68,6 +92,12 @@
                                             From: Zhuo, Xiaoyu, Alan Y Du, Erica C Pehrsson, Daofeng Li, and Ting Wang. "Epigenomic
                                             Differences in the Human and Chimpanzee Genomes Are Associated with Structural
                                             Variation." Genome Research, December 10, 2020, gr.263491.120. https://doi.org/10.1101/gr.263491.120.
+                                            <br />
+                                            <a
+                                                href="https://vizhub.wustl.edu/public/comparativeBrowser/demo/Fig5a.session.json"
+                                                target="_blank"
+                                                >Session file</a
+                                            >
                                         </li>
                                         <li>
                                             <a
@@ -81,6 +111,12 @@
                                             D Grinstein, Elie N Farah, Eugin Destici, et al. "Transcriptionally Active HERV-H
                                             Retrotransposons Demarcate Topologically Associating Domains in Human Pluripotent
                                             Stem Cells." 164 (August 2019): 1110. https://doi.org/10.1038/s41588-019-0479-7.
+                                            <br />
+                                            <a
+                                                href="https://vizhub.wustl.edu/public/comparativeBrowser/demo/Fig5b.session.json"
+                                                target="_blank"
+                                                >Session file</a
+                                            >
                                         </li>
                                         <li>
                                             <a
@@ -93,6 +129,46 @@
                                             and Duncan T Odom. "Waves of Retrotransposon Expansion Remodel Genome Organization
                                             and CTCF Binding in Multiple Mammalian Lineages." CELL 148, no. 1–2 (January
                                             2012): 335–48. https://doi.org/10.1016/j.cell.2011.11.058.
+                                            <br />
+                                            <a
+                                                href="https://vizhub.wustl.edu/public/comparativeBrowser/demo/Fig6.session.json"
+                                                target="_blank"
+                                                >Session file</a
+                                            >
+                                        </li>
+                                        <li>
+                                            <a
+                                                href="https://epigenomegateway.wustl.edu/browser/?sessionFile=https://vizhub.wustl.edu/public/comparativeBrowser/demo/Fig7a.session.json"
+                                                target="_blank">Comparing DNA methylation in heart, lung and liver between cow and mouse</a
+                                            >
+                                            <br />
+                                            From: Zhou, Yang, Shuli Liu, Yan Hu, Lingzhao Fang, Yahui Gao, Han Xia, 
+                                            Steven G. Schroeder, et al. “Comparative Whole Genome DNA Methylation 
+                                            Profiling across Cattle Tissues Reveals Global and Tissue-Specific Methylation Patterns.” 
+                                            BMC Biology 18, no. 1 (July 6, 2020): 85. https://doi.org/10/gpgpb2.
+
+                                            <br />
+                                            <a
+                                                href="https://vizhub.wustl.edu/public/comparativeBrowser/demo/Fig7a.session.json"
+                                                target="_blank"
+                                                >Session file</a
+                                            >
+                                        </li>
+                                        <li>
+                                            <a
+                                                href="https://epigenomegateway.wustl.edu/browser/?sessionFile=https://vizhub.wustl.edu/public/comparativeBrowser/demo/Fig7b.session.json"
+                                                target="_blank">DNA methylation calling difference between hg38 and CHM13 </a
+                                            >
+                                            <br />
+                                            From: Gershman, Ariel, Michael E. G. Sauria, Xavi Guitart, Mitchell R. Vollger, 
+                                            Paul W. Hook, Savannah J. Hoyt, Miten Jain, et al. “Epigenetic Patterns in a Complete Human Genome.” 
+                                            Science 376, no. 6588 (April 2022): eabj5089. https://doi.org/10.1126/science.abj5089.
+                                            <br />
+                                            <a
+                                                href="https://vizhub.wustl.edu/public/comparativeBrowser/demo/Fig7b.session.json"
+                                                target="_blank"
+                                                >Session file</a
+                                            >
                                         </li>
                                     </ol>
                                 </div>
