9e3adec929876328d657d19df64ebdc500e8cf53
xzhuo <yunkzo@gmail.com>
2023-02-14 13:59:56 -0600

    Download

--- src/components/Showcases.svelte
+++ src/components/Showcases.svelte
@@ -22,10 +22,10 @@
                                             of the DNA Methylome in Mouse and Zebrafish Uncovers High Interspecies Divergence.”
                                             Frontiers in Genetics 7 (June 17, 2016). https://doi.org/10.3389/fgene.2016.00110.
                                             <br />
-                                            <a
-                                                href="https://vizhub.wustl.edu/public/comparativeBrowser/demo/Fig3a.session.json"
-                                                target="_blank"
-                                                >Session file</a
+                                            <a 
+                                                href="#"
+                                                onclick="downloadFile('https://vizhub.wustl.edu/public/comparativeBrowser/demo/Fig3a.session.json', 'Fig3a.session.json'); event.preventDefault()"
+                                                >Download Session File</a
                                             >
                                         </li>
                                         <li>
@@ -41,10 +41,10 @@
                                             Richard Sandstrom, et al. “A Comparative Encyclopedia of DNA Elements in the Mouse Genome.”
                                             Nature 515, no. 7527 (November 2014): 355–64. https://doi.org/10.1038/nature13992.
                                             <br />
-                                            <a
-                                                href="https://vizhub.wustl.edu/public/comparativeBrowser/demo/Fig3b.session.json"
-                                                target="_blank"
-                                                >Session file</a
+                                            <a 
+                                                href="#"
+                                                onclick="downloadFile('https://vizhub.wustl.edu/public/comparativeBrowser/demo/Fig3b.session.json', 'Fig3b.session.json'); event.preventDefault()"
+                                                >Download Session File</a
                                             >
                                         </li>
                                         <li>
@@ -59,10 +59,10 @@
                                             “Enhancer Divergence and Cis-Regulatory Evolution in the Human and Chimp Neural Crest.”
                                             CELL 163, no. 1 (September 2015): 68–83. https://doi.org/10.1016/j.cell.2015.08.036.
                                             <br />
-                                            <a
-                                                href="https://vizhub.wustl.edu/public/comparativeBrowser/demo/Fig4a.session.json"
-                                                target="_blank"
-                                                >Session file</a
+                                            <a 
+                                                href="#"
+                                                onclick="downloadFile('https://vizhub.wustl.edu/public/comparativeBrowser/demo/Fig4a.session.json', 'Fig4a.session.json'); event.preventDefault()"
+                                                >Download Session File</a
                                             >
                                         </li>
                                         <li>
@@ -76,10 +76,10 @@
                                             “Comparing 3D Genome Organization in Multiple Species Using Phylo-HMRF.”
                                             Cell Systems 8, no. 6 (June 26, 2019): 494-505.e14. https://doi.org/10.1016/j.cels.2019.05.011.
                                             <br />
-                                            <a
-                                                href="https://vizhub.wustl.edu/public/comparativeBrowser/demo/Fig4b.session.json"
-                                                target="_blank"
-                                                >Session file</a
+                                            <a 
+                                                href="#"
+                                                onclick="downloadFile('https://vizhub.wustl.edu/public/comparativeBrowser/demo/Fig4b.session.json', 'Fig4b.session.json'); event.preventDefault()"
+                                                >Download Session File</a
                                             >
                                         </li>
                                         <li>
@@ -93,10 +93,10 @@
                                             Differences in the Human and Chimpanzee Genomes Are Associated with Structural
                                             Variation." Genome Research, December 10, 2020, gr.263491.120. https://doi.org/10.1101/gr.263491.120.
                                             <br />
-                                            <a
-                                                href="https://vizhub.wustl.edu/public/comparativeBrowser/demo/Fig5a.session.json"
-                                                target="_blank"
-                                                >Session file</a
+                                            <a 
+                                                href="#"
+                                                onclick="downloadFile('https://vizhub.wustl.edu/public/comparativeBrowser/demo/Fig5a.session.json', 'Fig5a.session.json'); event.preventDefault()"
+                                                >Download Session File</a
                                             >
                                         </li>
                                         <li>
@@ -112,10 +112,10 @@
                                             Retrotransposons Demarcate Topologically Associating Domains in Human Pluripotent
                                             Stem Cells." CELL, 164 (August 2019): 1110. https://doi.org/10.1038/s41588-019-0479-7.
                                             <br />
-                                            <a
-                                                href="https://vizhub.wustl.edu/public/comparativeBrowser/demo/Fig5b.session.json"
-                                                target="_blank"
-                                                >Session file</a
+                                            <a 
+                                                href="#"
+                                                onclick="downloadFile('https://vizhub.wustl.edu/public/comparativeBrowser/demo/Fig5b.session.json', 'Fig5b.session.json'); event.preventDefault()"
+                                                >Download Session File</a
                                             >
                                         </li>
                                         <li>
@@ -130,10 +130,10 @@
                                             and CTCF Binding in Multiple Mammalian Lineages." CELL 148, no. 1–2 (January
                                             2012): 335–48. https://doi.org/10.1016/j.cell.2011.11.058.
                                             <br />
-                                            <a
-                                                href="https://vizhub.wustl.edu/public/comparativeBrowser/demo/Fig6.session.json"
-                                                target="_blank"
-                                                >Session file</a
+                                            <a 
+                                                href="#"
+                                                onclick="downloadFile('https://vizhub.wustl.edu/public/comparativeBrowser/demo/Fig6.session.json', 'Fig6.session.json'); event.preventDefault()"
+                                                >Download Session File</a
                                             >
                                         </li>
                                         <li>
@@ -148,10 +148,10 @@
                                             BMC Biology 18, no. 1 (July 6, 2020): 85. https://doi.org/10/gpgpb2.
 
                                             <br />
-                                            <a
-                                                href="https://vizhub.wustl.edu/public/comparativeBrowser/demo/Fig7a.session.json"
-                                                target="_blank"
-                                                >Session file</a
+                                            <a 
+                                                href="#"
+                                                onclick="downloadFile('https://vizhub.wustl.edu/public/comparativeBrowser/demo/Fig7a.session.json', 'Fig7a.session.json'); event.preventDefault()"
+                                                >Download Session File</a
                                             >
                                         </li>
                                         <li>
@@ -164,10 +164,10 @@
                                             Paul W. Hook, Savannah J. Hoyt, Miten Jain, et al. “Epigenetic Patterns in a Complete Human Genome.” 
                                             Science 376, no. 6588 (April 2022): eabj5089. https://doi.org/10.1126/science.abj5089.
                                             <br />
-                                            <a
-                                                href="https://vizhub.wustl.edu/public/comparativeBrowser/demo/Fig7b.session.json"
-                                                target="_blank"
-                                                >Session file</a
+                                            <a 
+                                                href="#"
+                                                onclick="downloadFile('https://vizhub.wustl.edu/public/comparativeBrowser/demo/Fig7b.session.json', 'Fig7b.session.json'); event.preventDefault()"
+                                                >Download Session File</a
                                             >
                                         </li>
                                     </ol>
@@ -178,6 +178,21 @@
                 </div>
             </div>
         </section>
+        <script>
+            function downloadFile(url, filename) {
+                fetch(url)
+                    .then(response => response.blob())
+                    .then(blob => {
+                    const url = window.URL.createObjectURL(new Blob([blob]));
+                    const link = document.createElement('a');
+                    link.href = url;
+                    link.setAttribute('download', filename);
+                    document.body.appendChild(link);
+                    link.click();
+                    link.parentNode.removeChild(link);
+                    });
+            }
+        </script>
     </main>
     <!-- <Footer /> -->
 </div>
